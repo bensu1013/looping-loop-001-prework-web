81e49aa6778edae69f19b836a6c6d1a9e688a364
bensu1013 <bensu1013@hotmail.com>
2016-03-16 01:57:42 +0000

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,12 @@
 def looping
-	#your code here
+  counter = 0
+	loop do
+    puts "I am special, my mom told me!"
+    counter = counter + 1
+    if counter > 10
+      break
+    end
+  end
 end
 
-#call your method here
+looping
